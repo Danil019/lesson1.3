1641c4443df32294ef2fff1a743c892d82c1599f
Danil019 <100717092+Danil019@users.noreply.github.com>
2024-09-01 12:29:07 +0300

    Add files via upload

---
+++ lesson1.3.py
@@ -0,0 +1,10 @@
+#1 task
+complete_Home_Work = 12
+#2 task
+hoursExpended = 1.5
+#3 task
+course_name = 'Python'
+#4 task
+averageExecutionTime = hoursExpended / complete_Home_Work
+#Final
+print("Всего выполнено ДЗ:\n", complete_Home_Work, "\nЗатрачено часов:\n", hoursExpended, "\nКурс:\n", course_name, "\nСреднее время выполнения\n", averageExecutionTime, "часов")
